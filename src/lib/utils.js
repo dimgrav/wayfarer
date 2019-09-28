cdf0e73cecb81ea79700e98246ad5ee6d4b362c6
George Koutsikos <ragecryx@gmail.com>
2019-09-28 15:51:24 +0300

    Add Roll function in utils to eval expressions like 4d8+6

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,4 +1,33 @@
 import { isEqual } from "lodash";
+import Chance from "chance";
+
+
+const DiceRandom = new Chance();
+
+/**
+ * Rolls the expression and returns a single result.
+ *
+ * @param {String} expression A dice roll expression like 2D6+3
+ */
+export const Roll = (expression) => {
+    // Split by + - * / but keep the delimeter
+    const split = expression.split(/([\+\-\*\/])/g);
+
+    // Resolve all XdX e.g. 2d8 expressions using Change library
+    let resolveRolls = [];
+
+    for(let p of split) {
+        if (/^\d*d\d{1,20}$/img.test(p)) {
+            resolveRolls.push(DiceRandom.rpg(p.toLowerCase(), {sum: true}));
+        } else {
+            resolveRolls.push(p); // ... else we keep it
+        }
+    }
+
+    // Evaluate the fixed expression that remains (with rolls resolved)
+    // TODO: Replace eval with something that evaulates only math expressions for security reasons.
+    return eval(resolveRolls.join(''));
+};
 
 
 export const mapPropsToNavOptions = (props, prevProps, getPropsToMap) => {
